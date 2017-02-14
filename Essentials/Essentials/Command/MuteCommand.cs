b496df182597f82faa0ed2f3e698e58910ef06f8
푸른곡괭이 <sungwanim@gmail.com>
2017-02-15 02:51:47 +0900

    Update MuteCommand.cs

--- Essentials/Essentials/Command/MuteCommand.cs
+++ Essentials/Essentials/Command/MuteCommand.cs
@@ -1,4 +1,17 @@
-﻿using System;
+
+/*
+     ________  ___  _______   ________     
+    |\   __  \|\  \|\  ___ \ |\   __  \    
+    \ \  \|\  \ \  \ \   __/|\ \  \|\  \   
+     \ \   ____\ \  \ \  \_|/_\ \   __  \  
+      \ \  \___|\ \  \ \  \_|\ \ \  \ \  \ 
+       \ \__\    \ \__\ \_______\ \__\ \__\
+        \|__|     \|__|\|_______|\|__|\|__|          
+    
+    PIEA, The MiNET plugins development organization.                                  
+*/
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
