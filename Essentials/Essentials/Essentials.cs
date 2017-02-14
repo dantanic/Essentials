64b066fa7a4615263c15b8d702a82d978c41a1eb
Herb9 <pksxtm@gmail.com>
2017-02-14 22:08:25 +0900

    Gamemode 클래스 추가.

---
+++ Essentials/Essentials/Essentials.cs
@@ -0,0 +1,24 @@
+﻿
+/*
+     ________  ___  _______   ________     
+    |\   __  \|\  \|\  ___ \ |\   __  \    
+    \ \  \|\  \ \  \ \   __/|\ \  \|\  \   
+     \ \   ____\ \  \ \  \_|/_\ \   __  \  
+      \ \  \___|\ \  \ \  \_|\ \ \  \ \  \ 
+       \ \__\    \ \__\ \_______\ \__\ \__\
+        \|__|     \|__|\|_______|\|__|\|__|          
+    
+    PIEA, The MiNET plugins development organization.                                  
+*/
+
+
+namespace Essentials
+{
+    public class Essentials
+    {
+        public Essentials()
+        {
+
+        }
+    }
+}
---
+++ Essentials/Essentials/PluginLoader.cs
@@ -0,0 +1,34 @@
+﻿
+/*
+     ________  ___  _______   ________     
+    |\   __  \|\  \|\  ___ \ |\   __  \    
+    \ \  \|\  \ \  \ \   __/|\ \  \|\  \   
+     \ \   ____\ \  \ \  \_|/_\ \   __  \  
+      \ \  \___|\ \  \ \  \_|\ \ \  \ \  \ 
+       \ \__\    \ \__\ \_______\ \__\ \__\
+        \|__|     \|__|\|_______|\|__|\|__|          
+    
+    PIEA, The MiNET plugins development organization.                                  
+*/
+
+using MiNET.Plugins;
+using MiNET.Plugins.Attributes;
+
+using Essentials.Command;
+
+namespace Essentials
+{
+    [Plugin(PluginName = "Essentials", Description = "에센셜 플러그인", PluginVersion = "1.0", Author = "PIEA Organization")]
+    public class PluginLoader : Plugin
+    {
+        protected override void OnEnable()
+        {
+            RegisterCommands();
+        }
+
+        private void RegisterCommands()
+        {
+            Context.PluginManager.LoadCommands(new Gamemode(new Essentials()));
+        }
+    }
+}
