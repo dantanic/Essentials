12a1241dc95cd1e1e77e78a187002da2578c3937
BluePickaxe <sungwanim@naver.com>
2017-02-18 02:07:11 +0900

    gamemode 단축명령어(gm) 추가

--- Essentials/Essentials/Command/GamemodeCommand.cs
+++ Essentials/Essentials/Command/GamemodeCommand.cs
@@ -92,5 +92,67 @@ namespace Essentials.Command
                 sender.SendMessage(ChatColors.Yellow + StringResources.Gamemode_PlayerNotFound);
             }
         }
+        [Command]
+        public void gm(Player sender)
+        {
+            if (!PermissionManager.Manager.CheckCurrentUserPermission(sender))
+            {
+                return;
+            }
+
+            sender.SendMessage($"{ChatColors.Green}/gm [gamemode number] -> " + StringResources.Gamemode_Help1);
+            sender.SendMessage($"{ChatColors.Green}/gm [gamemode number] [target name] -> " + StringResources.Gamemode_Help2);
+        }
+
+        [Command]
+        public void gm(Player sender, int gamemodeValue)
+        {
+            if (!PermissionManager.Manager.CheckCurrentUserPermission(sender))
+            {
+                return;
+            }
+
+            if (Enum.IsDefined(typeof(GameMode), gamemodeValue))
+            {
+                var gameModeSet = (GameMode)gamemodeValue;
+                sender.SetGameMode(gameModeSet);
+                sender.SendMessage($"{ChatColors.Green}{StringResources.Gamemode_DisplayCompletedResult.Replace("{{value}}", gameModeSet.ToString())}");
+            }
+            else
+            {
+                sender.SendMessage($"{ChatColors.Yellow}{StringResources.Gamemode_InvaildGameModeValue.Replace("{{value}}", gamemodeValue.ToString())}");
+            }
+        }
+
+        [Command]
+        public void gm(Player sender, int gamemodeValue, string targetName)
+        {
+            if (!PermissionManager.Manager.CheckCurrentUserPermission(sender))
+            {
+                return;
+            }
+
+            var ServerPlayers = sender.Level.Players;
+            var targetPlayer = ServerPlayers.ToList().Find(x => x.Value.Username == targetName).Value;
+            if (targetPlayer == null)
+            {
+                sender.SendMessage($"{ChatColors.Yellow}{StringResources.Gamemode_PlayerNotFound}");
+                return;
+            }
+
+            if (Enum.IsDefined(typeof(GameMode), gamemodeValue))
+            {
+                var gameModeSet = (GameMode)gamemodeValue;
+                targetPlayer.SetGameMode(gameModeSet);
+
+                var msg_player = StringResources.Gamemode_DisplayCompletedResult_Target.Replace("{{target}}", targetPlayer.Username);
+                msg_player = msg_player.Replace("{{gamemode}}", gameModeSet.ToString());
+                sender.SendMessage(ChatColors.Green + msg_player);
+            }
+            else
+            {
+                sender.SendMessage(ChatColors.Yellow + StringResources.Gamemode_PlayerNotFound);
+            }
+        }
     }
 }
