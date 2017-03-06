8f667712e95d38b8125fc96bdb233f90dc4cf0f3
BluePickaxe <sungwanim@naver.com>
2017-03-06 22:53:52 +0900

    Up 커맨드 추가(텔레포트 후 발밑에 유리블럭 생기게 하는것은 구현 못함.)

---
+++ Essentials/Essentials/Command/Up.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MiNET;
+using MiNET.Plugins.Attributes;
+using MiNET.Utils;
+
+namespace Essentials.Command
+{
+    public class Up
+    {
+        [Command(Name = "up")]
+        public void Execute(Player sender, int y)
+        {
+            var kp = sender.KnownPosition;
+            var pos = new PlayerLocation()
+            {
+                X = kp.X,
+                Y = kp.Y,
+                Z = kp.Z
+            };
+            var py = y + pos.Y;
+            sender.Teleport(new PlayerLocation()
+            {
+                X = kp.X,
+                Y = py,
+                Z = kp.Z
+            });
+        }
+    }
+}
