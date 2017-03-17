efff5e56c5bc797f38b48ee1de687b3cde9d6287
BluePickaxe <sungwanim@naver.com>
2017-03-17 21:36:32 +0900

    다른 사람의 fly 상태까지 변경 가능하게 수정 및 miop 추가

--- Essentials/Essentials/Command/Fly.cs
+++ Essentials/Essentials/Command/Fly.cs
@@ -14,6 +14,7 @@
 using MiNET;
 using MiNET.Plugins.Attributes;
 using MiNET.Utils;
+using MiOP;
 
 namespace Essentials.Command
 {
@@ -29,13 +30,34 @@ namespace Essentials.Command
         [Command(Name = "fly", Description = "Allow flying.")]
         public void Execute(Player sender)
         {
-            if (sender.AllowFly == false) 
+            if (!sender.AllowFly)
             {
-               sender.AllowFly = true;
-               sender.SendMessage(ChatColors.Green + "You can fly.");
-            } else {
-               sender.AllowFly = false;
-               sender.SendMessage(ChatColors.Green + "You can't fly.");
+                sender.AllowFly = true;
+                sender.SendMessage($"{ChatColors.Green}You can fly.");
+            }
+            else
+            {
+                sender.AllowFly = false;
+                sender.SendMessage($"{ChatColors.Green}You can't fly.");
+            }
+        }
+        [Command(Name = "fly", Description = "Allow flying.")]
+        public void Execute(Player sender, string name)
+        {
+            if (!PermissionManager.Manager.CheckCurrentUserPermission(sender)) return;
+            var gp = Plugin.GetPlayer(name);
+            if (gp == null) sender.SendMessage("플레이어가 존재하지 않습니다.");
+            if (!gp.AllowFly)
+            {
+                gp.AllowFly = true;
+                sender.SendMessage($"{ChatColors.Green}{name} can fly.");
+                gp.SendMessage($"{ChatColors.Green}You can't fly.");
+            }
+            else
+            {
+                gp.AllowFly = false;
+                sender.SendMessage($"{ChatColors.Green}{name} can't fly.");
+                gp.SendMessage($"{ChatColors.Green}You can't fly.");
             }
         }
     }
--- Essentials/Essentials/Command/Lightning.cs
+++ Essentials/Essentials/Command/Lightning.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using MiNET;
-using MiNET.Plugins;
+﻿using MiNET;
 using MiNET.Plugins.Attributes;
+using MiOP;
 
 namespace Essentials.Command
 {
@@ -26,11 +21,9 @@ namespace Essentials.Command
         [Command(Name = "lightning")]
         public void Execute(Player sender, string name)
         {
+            if (!PermissionManager.Manager.CheckCurrentUserPermission(sender)) return;
             var gp = Plugin.GetPlayer(name);
-            if (gp == null)
-            {
-                sender.SendMessage("플레이어가 존재하지 않습니다.");
-            }
+            if (gp == null) sender.SendMessage("플레이어가 존재하지 않습니다.");
             gp.StrikeLightning();
             sender.SendMessage("lightning!!!");
             gp.SendMessage("lightning!!!");
